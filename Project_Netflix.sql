7159ef19fdfd9788a7a2b3cf802c4f7a01db7bc8
EvgeniyShchepelin <evgeniyginsane@gmail.com>
2020-10-28 21:32:54 +0300

    update

--- Project_Netflix.sql
+++ Project_Netflix.sql
@@ -11436,58 +11436,39 @@ group by u.account_email
 order by u.account_email
 ;
 
--- Триггер 2
--- Сделать триггер, который делает проверку из запроса выше и не дает создать лишний профайл для аккаунта (исходя из лимита подписки)
+-- Триггер 2 - делает проверку из запроса выше и не дает создать лишний профайл для аккаунта (исходя из лимита подписки)
 
-DROP TRIGGER IF EXISTS new_profiles_limit;
+DROP TRIGGER IF EXISTS check_accounts_count;
+
 delimiter //
 
-CREATE TRIGGER new_profiles_limit BEFORE INSERT ON profiles 
-FOR EACH ROW
+create trigger check_accounts_count 
+before insert 
+on profiles -- отслеживаем попытку добавить новый профиль
+for each row
 begin 
-DECLARE profiles_count INT, profiles_limit INT; -- объявляем 2 переменных для проверки
-
--- записываем количество профайлов у введенного юзера в переменную profiles_count
-select count(p.profile_nickname) into profiles_count
-from profiles p
-join users u
-on p.account = u.account_id
-where p.profile_nickname = new.profile_nickname
-;
-
--- записываем доступный лимит профайлов у введенного юзера в переменную profiles_limit
-select
-case 
-	when dos.subcsription_id = '1' then 8
-	when dos.subcsription_id = '2' then 4
-	when dos.subcsription_id = '3' then 2
+	
+	declare accounts_count int; -- объявляем переменную для кол-ва аккаунтов
+	declare accounts_limit int;-- объявляем переменную для кол-ва аккаунтов
+	
+	-- получаем кол-во доступных аккаунтов
+	select 
+	case 
+		when subscription_id = '1' then 8
+		when subscription_id = '2' then 4
+		when subscription_id = '3' then 2
 	end
-into profiles_limit
-from profiles p
-join users u 
-on p.account = u.account_id
-join details_of_subcsription dos 
-on u.subscription_id = dos.subcsription_id
-where p.profile_nickname = new.profile_nickname
-;
+	into accounts_limit
+	from users
+	where account_id = new.account;
 
--- Добавляем проверку лимита добавления через IF
-    IF profiles_count >= profiles_limit then   
-    
--- Тут нужно добавить команду НЕ добавления новой строки    
-        UPDATE 
-        SET 
-       ;
-    else
--- Тут нужно добавить команду добавления новой строки  
-        INSERT INTO 
-        VALUES(new.);
-
-
-INSERT INTO `profiles` 
-SET 
--- тут непонятно что написать
-;
-end //
+	-- получаем кол-во существующих аккаунтов
+	select count(*) into accounts_count from profiles where account = new.account;
+	-- проверяем, чтобы доступных аккаунтом было меньше, чем существующих, 
+	-- если это не так, выдаём ошибку, данные не добавляются
+	if accounts_limit <= accounts_count then 
+		signal sqlstate '45000' set message_text = 'Количество учётных записей по тарифному плану превышено!';
+	end if;
+end//
 
 delimiter ;
--- Queries netflix.sql
+++ Queries netflix.sql
@@ -196,3 +196,39 @@ group by u.account_email
 order by u.account_email
 ;
 
+-- Триггер 2 - делает проверку из запроса выше и не дает создать лишний профайл для аккаунта (исходя из лимита подписки)
+
+DROP TRIGGER IF EXISTS check_accounts_count;
+
+delimiter //
+
+create trigger check_accounts_count 
+before insert 
+on profiles -- отслеживаем попытку добавить новый профиль
+for each row
+begin 
+	
+	declare accounts_count int; -- объявляем переменную для кол-ва аккаунтов
+	declare accounts_limit int;-- объявляем переменную для кол-ва аккаунтов
+	
+	-- получаем кол-во доступных аккаунтов
+	select 
+	case 
+		when subscription_id = '1' then 8
+		when subscription_id = '2' then 4
+		when subscription_id = '3' then 2
+	end
+	into accounts_limit
+	from users
+	where account_id = new.account;
+
+	-- получаем кол-во существующих аккаунтов
+	select count(*) into accounts_count from profiles where account = new.account;
+	-- проверяем, чтобы доступных аккаунтом было меньше, чем существующих, 
+	-- если это не так, выдаём ошибку, данные не добавляются
+	if accounts_limit <= accounts_count then 
+		signal sqlstate '45000' set message_text = 'Количество учётных записей по тарифному плану превышено!';
+	end if;
+end//
+
+delimiter ;
